cb2ba5d152fab112eaa3cb764d3b4220baeca40b
Zulma Rocio <zrmartinezg@gmail.com>
2021-08-18 18:02:53 -0500

    Initialize repo

---
+++ index.js
@@ -0,0 +1,23 @@
+String.prototype.reverse = function reverse(){
+  return Array.from(this).reverse().join("");
+}
+
+
+function Phrase(content){
+  this.content = content;
+  this.processor = function processor(str){
+    return str.toLowerCase();
+  }
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+  
+  this.palindrome = function palindrome(){
+     return this.processedContent() === (this.processedContent()).reverse();
+  }
+ 
+}
+
+
+
+ 
