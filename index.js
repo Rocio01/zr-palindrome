0ffd21b9d53f08e49e87ed5d7d32733151ec220a
Zulma Rocio <zrmartinezg@gmail.com>
2021-08-20 12:34:00 -0500

    Refractor palindrome module

--- index.js
+++ index.js
@@ -7,46 +7,19 @@ String.prototype.reverse = function reverse(){
 
 function Phrase(content){
   this.content = content;
-
+  
   this.letters = function letters(){
-  //  let theLetters = [];
    let lettersRegex = /[a-z]/gi;
-
     return (this.content.match(lettersRegex) || []).join("");
-
-    //  functional programming
-
-    // return Array.from(this.content).filter(c => c.match(lettersRegex)).join("");
-
-    //  with a forEach loop
-
-    // Array.from(this.content).forEach((character) => {
-    //   if (character.match(lettersRegex)){
-    //     theLetters.push(character);
-    //   }
-    // } )
-
-    // with a for loop
-
-    // for(let i = 0; i < this.content.length; i ++){
-    //   let character = this.content.charAt(i);
-    //   if (character.match(lettersRegex)){
-    //     theLetters.push(character);
-    //   }
-    // }
-
-    // return theLetters.join("");
   }
 
   this.processedContent = function processedContent() {
     return this.letters().toLowerCase();
   }
-
   
   this.palindrome = function palindrome(){
      return this.processedContent() === (this.processedContent()).reverse();
-  }
- 
+  } 
 }
 
 
